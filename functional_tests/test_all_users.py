7039da60aac36cf2550dfd314dddeb5bc4c69512
Ugonna Erinne <ue24@cornell.edu>
2016-06-09 17:53:52 -0500

    Gave up on localization and internationalization.  Sphinx installed and configured.

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from datetime import date
+from django.utils import formats
  
  
 class HomeNewVisitorTest(StaticLiveServerTestCase): 
@@ -25,6 +27,31 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         h1 = self.browser.find_element_by_tag_name("h1")
         self.assertEqual(h1.value_of_css_property("color"), 
                          "rgba(200, 50, 255, 1)")
+
+    def test_home_files(self):
+        self.browser.get(self.live_server_url + "/robots.txt")
+        self.assertNotIn("Not Found", self.browser.title)
+        self.browser.get(self.live_server_url + "/humans.txt")
+        self.assertNotIn("Not Found", self.browser.title)
+
+    def test_internationalization(self):
+    for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
+                                ('ca', 'Benvingut a TaskBuster!')]:
+        activate(lang)
+        self.browser.get(self.get_full_url("home"))
+        h1 = self.browser.find_element_by_tag_name("h1")
+        self.assertEqual(h1.text, h1_text)
+
+    def test_localization(self):
+    today = date.today()
+    for lang in ['en', 'ca']:
+        activate(lang)
+        self.browser.get(self.get_full_url("home"))
+        local_date = self.browser.find_element_by_id("local-date")
+        non_local_date = self.browser.find_element_by_id("non-local-date")
+        self.assertEqual(formats.date_format(today, use_l10n=True),
+                              local_date.text)
+        self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
  
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -71,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -103,14 +105,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+from django.utils.translation import ugettext_lazy as _
+LANGUAGES = (
+    ('en', _('English')),
+    ('ca', _('Catalan')),
+)
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
-2
-3
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
